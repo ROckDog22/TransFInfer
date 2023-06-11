1d6b7cc8207b7e02f31d51e53baca3837b8af197
vooolta <wangfengsheng1999@gmail.com>
2023-06-11 10:22:12 +0000

    first commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include <glog/logging.h>
+
+int main(int argc, char *argv[]) {
+    testing::InitGoogleTest(&argc, argv);
+    google::InitGoogleLogging("TransFInfer");
+    FLAGS_log_dir = "./log";
+    FLAGS_alsologtostderr = true;
+    LOG(INFO) << "Start test ... \n";
+    return RUN_ALL_TESTS();
+}
